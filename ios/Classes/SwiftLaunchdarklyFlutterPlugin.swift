030b7bbe5519c1cd2aeebc9babada8f618d93a3a
Andre Paraense <andre.paraense@oakam.com>
2020-01-28 14:36:15 +0000

    iOS Platform channel

--- ios/Classes/SwiftLaunchdarklyFlutterPlugin.swift
+++ ios/Classes/SwiftLaunchdarklyFlutterPlugin.swift
@@ -12,16 +12,63 @@ import LaunchDarkly
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult)  {
+    let arguments = call.arguments as! [String:Any]
+    
     if (call.method == "init") {
+        
+        let mobileKey = arguments["mobileKey"] as? String
+        
+        if (mobileKey ?? "").isEmpty {
+            result(false)
+        }
+        
+        let config = LDConfig(mobileKey: mobileKey ?? "")
+        
+        let userKey = arguments["userKey"] as? String
+        let userEmail = arguments["userEmail"] as? String
+        
+        if( (userKey ?? "").isEmpty || (userEmail ?? "").isEmpty) {
+            
+            LDClient.shared.startCompleteWhenFlagsReceived(config: config)
+            
+        }else{
+            
+            let user = LDUser(key: userKey, email: userEmail)
+            LDClient.shared.startCompleteWhenFlagsReceived(config: config, user: user)
+        }
+        
+        result(true)
 
     } else if(call.method == "boolVariation") {
+        
+        let flagKey = arguments["flagKey"] as? String ?? ""
+        
+        result(LDClient.shared.variation(forKey: flagKey, fallback: false))
 
     } else if (call.method == "boolVariationFallback") {
+        
+        let flagKey = arguments["flagKey"] as? String ?? ""
+        
+        var fallback = arguments["fallback"] as? Bool ?? false
+        
+        result(LDClient.shared.variation(forKey: flagKey, fallback: fallback) as Bool)
 
     } else if(call.method == "stringVariation") {
+        
+        let flagKey = arguments["flagKey"] as? String ?? ""
+        
+        result(LDClient.shared.variation(forKey: flagKey, fallback: ""))
 
     } else if(call.method == "stringVariationFallback") {
-
+        
+        let flagKey = arguments["flagKey"] as? String ?? ""
+               
+        var fallback = arguments["fallback"] as? String ?? ""
+               
+        result(LDClient.shared.variation(forKey: flagKey, fallback: fallback) as String)
+        
+    } else {
+        result(FlutterMethodNotImplemented)
     }
   }
 }
